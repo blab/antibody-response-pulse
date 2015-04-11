d05e6d7500e4831aa67fe600589430fe169a7cbf
Alvason Li <al@DHCP155016.FHCRC.ORG>
2015-04-10 17:08:54 -0700

    antibody response for 1st and 2nd infections

--- bcell-array/bcell-array.py
+++ bcell-array/bcell-array.py
@@ -7,6 +7,7 @@
 # https://github.com/blab/antibody-response-pulse
 # 
 # ### B-cells evolution --- cross-reactive antibody response after influenza virus infection or vaccination
+# ### Adaptive immunity has memory --- primary and secondary immune response
 
 # <codecell>
 
@@ -25,44 +26,47 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 import alva_machinery as alva
 
 AlvaFontSize = 23;
-AlvaFigSize = (9, 7);
+AlvaFigSize = (14, 6);
 numberingFig = 0;
 
 numberingFig = numberingFig + 1;
 plt.figure(numberingFig, figsize=(12,6))
 plt.axis('off')
-plt.title(r'$ B-cells \ cross-reactive \ antibody \ response \ equations $',fontsize = AlvaFontSize)
-plt.text(0,5.0/6,r'$ \frac{\partial T_n(t)}{\partial t} = \
-         -\beta T_n(t)V_n(t) - \phi F_{n}(t)T_{n}(t) +\mu R_n(t) $', fontsize = 1.2*AlvaFontSize)
-plt.text(0,4.0/6,r'$ \frac{\partial I_n(t)}{\partial t} = \
-         +\beta T_n(t)I(t) - \gamma I_n(t) - \xi I_{n}(t)F_{n}(t) $'
+plt.title(r'$ Virion-Tcell-Bcell-Antibody \ response \ equations \ (primary-infection) $',fontsize = AlvaFontSize)
+plt.text(0, 5.0/6,r'$ \frac{\partial V_n(t)}{\partial t} = \
+         +\rho V_n(t)(1 - \frac{V_n(t)}{\Omega}) - \phi_v A_{n}(t)V_{n}(t) $', fontsize = 1.2*AlvaFontSize)
+plt.text(0, 4.0/6,r'$ \frac{\partial T_n(t)}{\partial t} = \
+         +\mu_t + \alpha_t V_n(t)T_{n}(t) - \mu_t T_{n}(t) $'
          , fontsize = 1.2*AlvaFontSize)
-plt.text(0,3.0/6,r'$ \frac{\partial R_n(t)}{\partial t} = \
-         +\phi F_{n}(t)T_{n}(t) - \mu R_n(t) $', fontsize = 1.2*AlvaFontSize)
-plt.text(0,2.0/6,r'$ \frac{\partial V_n(t)}{\partial t} = \
-         +\rho I_{n}(t) - c V_n(t) $', fontsize = 1.2*AlvaFontSize)
-plt.text(0,1.0/6,r'$ \frac{\partial F_n(t)}{\partial t} = \
-         +\lambda I_{n}(t) - \eta V_n(t) $', fontsize = 1.2*AlvaFontSize)
+plt.text(0, 3.0/6,r'$ \frac{\partial B_n(t)}{\partial t} = \
+         +\mu_b + \alpha_b V_{n}(t)T_{n}(t)B_{n}(t) - \mu_b B_n(t) $', fontsize = 1.2*AlvaFontSize)
+plt.text(0, 2.0/6,r'$ \frac{\partial A_n(t)}{\partial t} = \
+         +\xi B_{n}(t) - \phi_a A_{n}(t)V_{n}(t) - \mu_a A_{n}(t) $', fontsize = 1.2*AlvaFontSize)
 plt.show()
 
 # <codecell>
 
 # setting parameter
-timeUnit = 'year'
-if timeUnit == 'day':
-    day = 1; year = 365; 
-elif timeUnit == 'year':
-    year = 1; day = float(1)/365; 
+timeUnit = 'hour'
+if timeUnit == 'hour':
+    hour = 1; day = 24; 
+elif timeUnit == 'day':
+    day = 1; hour = float(1)/24; 
     
-totalSIR = float(1); # total population
-reprodNum = 1.8 # basic reproductive number R0: one infected person will transmit to 1.8 person 
-recovRate = float(1)/(4*day) # 4 days per period ==> rate/year = 365/4
-infecRate = reprodNum*recovRate/totalSIR # per year, per person, per total-population
-inOutRate = float(1)/(30*year) # birth rate per year
-mutatRate = float(3)/10**3 # mutation rate
+totalV = float(5000) # total virion/micro-liter
+inRateV = float(0.16) # intrinsic growth rate/hour
+outRateV = float(1.67*10**(-4)) # virion clearance rate by IgM
+inOutRateC = float(0.017) # birth rate of CD4 T-cell
+inOutRateB = float(0.037) # birth rate of B-cell
+inRateA = float(0.3) # growth rate of antibody from B-cell (secretion)
+outRateAm = float(0.014) # out rate of Antibody IgM
+outRateAmV = float(4.2*10**(-5)) # antibody IgM clearance rate by virus
+actRateC = float(7.0*10**(-6)) # activation rate of CD4 T-cell
+actRateB = float(6.0*10**(-7)) # activation rate of naive B-cell
+
 
 # time boundary and griding condition
-minT = float(0); maxT = float(1*year);
+minT = float(0); maxT = float(600*hour);
 totalGPoint_T = int(10**3 + 1);
 gridT = np.linspace(minT, maxT, totalGPoint_T);
 spacingT = np.linspace(minT, maxT, num = totalGPoint_T, retstep = True)
@@ -71,166 +75,197 @@ dt = spacingT[1]
 
 # space boundary and griding condition
 minX = float(0); maxX = float(1);
-totalGPoint_X = int(10 + 1);
+totalGPoint_X = int(1 + 1);
 gridX = np.linspace(minX, maxX, totalGPoint_X);
 gridingX = np.linspace(minX, maxX, num = totalGPoint_X, retstep = True)
 gridX = gridingX[0]
 dx = gridingX[1]
 
-gridS_array = np.zeros([totalGPoint_X, totalGPoint_T])
-gridI_array = np.zeros([totalGPoint_X, totalGPoint_T])
-gridR_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridV_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridC_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridB_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridA_array = np.zeros([totalGPoint_X, totalGPoint_T])
 
 # initial output condition
-gridI_array[0:1, 0] = float(1)/10**6
-gridR_array[0, 0] = float(0)
-gridS_array[:, 0] = totalSIR - gridI_array[:, 0] - gridR_array[:, 0]
+gridV_array[0:1, 0] = float(totalV)/10**3
+gridC_array[0, 0] = float(0)
+gridB_array[:, 0] = float(0)
+gridA_array[:, 0] = float(0)
 
-def dSdt_array(SIRxt = [], *args):
+def dVdt_array(VCBAxt = [], *args):
     # naming
-    S = SIRxt[0]
-    I = SIRxt[1]
-    R = SIRxt[2]
-    x_totalPoint = SIRxt.shape[1]
+    V = VCBAxt[0]
+    C = VCBAxt[1]
+    B = VCBAxt[2]
+    A = VCBAxt[3]
+    x_totalPoint = VCBAxt.shape[1]
     # there are n dSdt
-    dS_dt_array = np.zeros(x_totalPoint)
-    # each dSdt with the same equation form
+    dV_dt_array = np.zeros(x_totalPoint)
+    # each dVdt with the same equation form
     for xn in range(x_totalPoint):
-        dS_dt_array[xn] = -infecRate*S[xn]*I[xn] + inOutRate*totalSIR - inOutRate*S[xn]
-    return(dS_dt_array)
-def dIdt_array(SIRxt = [], *args):
+        dV_dt_array[xn] = +inRateV*V[xn]*(1 - V[xn]/totalV) - outRateV*A[xn]*V[xn]
+    return(dV_dt_array)
+
+def dCdt_array(VCBAxt = [], *args):
     # naming
-    S = SIRxt[0]
-    I = SIRxt[1]
-    R = SIRxt[2]
-    x_totalPoint = SIRxt.shape[1]
-    # there are n dIdt
-    dI_dt_array = np.zeros(x_totalPoint)
-    # each dIdt with the same equation form
-    Icopy = np.copy(I)
-    centerX = Icopy[:]
-    leftX = np.roll(Icopy[:], 1)
-    rightX = np.roll(Icopy[:], -1)
-    leftX[0] =centerX[0]
-    rightX[-1] = centerX[-1]
+    V = VCBAxt[0]
+    C = VCBAxt[1]
+    B = VCBAxt[2]
+    A = VCBAxt[3]
+    x_totalPoint = VCBAxt.shape[1]
+    # there are n dSdt
+    dC_dt_array = np.zeros(x_totalPoint)
+    # each dCdt with the same equation form
     for xn in range(x_totalPoint):
-        dI_dt_array[xn] = +infecRate*S[xn]*I[xn] - recovRate*I[xn] - inOutRate*I[xn] + mutatRate*(leftX[xn]
-                                                                                                  - 2*centerX[xn]
-                                                                                                  + rightX[xn])/(dx**2)
-    return(dI_dt_array)
-def dRdt_array(SIRxt = [], *args):
+        dC_dt_array[xn] = +inOutRateC + actRateC*V[xn]*C[xn] - inOutRateC*C[xn]
+    return(dC_dt_array)
+
+def dBdt_array(VCBAxt = [], *args):
     # naming
-    S = SIRxt[0]
-    I = SIRxt[1]
-    R = SIRxt[2]
-    x_totalPoint = SIRxt.shape[1]
-    # there are n dRdt
-    dR_dt_array = np.zeros(x_totalPoint)
-    # each dIdt with the same equation form
+    V = VCBAxt[0]
+    C = VCBAxt[1]
+    B = VCBAxt[2]
+    A = VCBAxt[3]
+    x_totalPoint = VCBAxt.shape[1]
+    # there are n dSdt
+    dB_dt_array = np.zeros(x_totalPoint)
+    # each dTdt with the same equation form
     for xn in range(x_totalPoint):
-        dR_dt_array[xn] = +recovRate*I[xn] - inOutRate*R[xn]
-    return(dR_dt_array)
+        dB_dt_array[xn] = +inOutRateB + actRateB*V[xn]*C[xn]*B[xn] - inOutRateB*B[xn]
+    return(dB_dt_array)
+
+def dAdt_array(VCBAxt = [], *args):
+    # naming
+    V = VCBAxt[0]
+    C = VCBAxt[1]
+    B = VCBAxt[2]
+    A = VCBAxt[3]
+    x_totalPoint = VCBAxt.shape[1]
+    # there are n dSdt
+    dA_dt_array = np.zeros(x_totalPoint)
+    # each dTdt with the same equation form
+    for xn in range(x_totalPoint):
+        dA_dt_array[xn] = +inRateA*B[xn] - outRateAmV*A[xn]*V[xn] - outRateAm*A[xn]
+    return(dA_dt_array)
 
 # Runge Kutta numerical solution
-pde_array = np.array([dSdt_array, dIdt_array, dRdt_array])
-startingOut_Value = np.array([gridS_array, gridI_array, gridR_array])
+pde_array = np.array([dVdt_array, dCdt_array, dBdt_array, dAdt_array])
+startingOut_Value = np.array([gridV_array, gridC_array, gridB_array, gridA_array])
 gridOut_array = alva.AlvaRungeKutta4ArrayXT(pde_array, startingOut_Value, minX, maxX, totalGPoint_X, minT, maxT, totalGPoint_T)
 
 # plotting
-gridS = gridOut_array[0]  
-gridI = gridOut_array[1]
-gridR = gridOut_array[2]
+gridV = gridOut_array[0]  
+gridC = gridOut_array[1]
+gridB = gridOut_array[2]
+gridA = gridOut_array[3]
+firstT = np.copy(gridT)
+firstA= np.copy(gridA)
 
 numberingFig = numberingFig + 1;
-plt.figure(numberingFig, figsize = AlvaFigSize)
-plt.contourf(gridT, gridX, gridI, levels = np.arange(0, 0.12, 0.001))
-plt.title(r'$ Infectious \ pulse \ I(x,t) \ by \ mutation $', fontsize = AlvaFontSize)
-plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize);
-plt.ylabel(r'$ discrete \ space \ (strain) $', fontsize = AlvaFontSize);
-plt.colorbar()
-plt.text(maxT*4.0/3, maxX*5.0/6, r'$ R_0 = %f $'%(reprodNum), fontsize = AlvaFontSize)
-plt.text(maxT*4.0/3, maxX*4.0/6, r'$ \gamma = %f $'%(recovRate), fontsize = AlvaFontSize)
-plt.text(maxT*4.0/3, maxX*3.0/6, r'$ \beta = %f $'%(infecRate), fontsize = AlvaFontSize)
-plt.text(maxT*4.0/3, maxX*2.0/6, r'$ \mu = %f $'%(inOutRate), fontsize = AlvaFontSize)
-plt.text(maxT*4.0/3, maxX*1.0/6, r'$ m = %f $'%(mutatRate), fontsize = AlvaFontSize)
-plt.show()
-
-numberingFig = numberingFig + 1;
-plt.figure(numberingFig, figsize = AlvaFigSize)
-plt.plot(gridT, gridS.T)
-plt.plot(gridT, gridR.T)
-plt.plot(gridT, gridI.T)
-plt.plot(gridT, (gridS + gridI + gridR).T, label = r'$ S(t)+I(t)+R(t) $', color = 'black')
-plt.title(r'$ Many-strain \ SIR $', fontsize = AlvaFontSize)
-plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize);
-plt.ylabel(r'$ Proportion \ of \ population $', fontsize = AlvaFontSize);
-plt.text(maxT, totalSIR*6.0/6, r'$ R_0 = %f $'%(reprodNum), fontsize = AlvaFontSize)
-plt.text(maxT, totalSIR*5.0/6, r'$ \gamma = %f $'%(recovRate), fontsize = AlvaFontSize)
-plt.text(maxT, totalSIR*4.0/6, r'$ \beta = %f $'%(infecRate), fontsize = AlvaFontSize)
-plt.text(maxT, totalSIR*3.0/6, r'$ \mu = %f $'%(inOutRate), fontsize = AlvaFontSize)
-plt.text(maxT, totalSIR*2.0/6, r'$ m = %f $'%(mutatRate), fontsize = AlvaFontSize)
-plt.show()
-
-# <codecell>
-
-'''
-# plot by listing each strain 
-numberingFig = numberingFig + 1;
-for i in range(totalGPoint_X):
+for i in range(1):
     plt.figure(numberingFig, figsize = AlvaFigSize)
-    plt.plot(gridT, gridS[i], label = r'$ S_{%i}(t) $'%(i))
-    plt.plot(gridT, gridR[i], label = r'$ R_{%i}(t) $'%(i))
-    plt.plot(gridT, gridI[i], label = r'$ I_{%i}(t) $'%(i))
-    plt.plot(gridT, infecRate*gridS[i].T*gridI[i].T*day, label = r'$ \beta \ S_{%i}(t)I_{%i}(t) $'%(i, i)
-             , linestyle = 'dashed', color = 'red')
-    plt.plot(gridT, (gridS[i] + gridI[i] + gridR[i]).T, label = r'$ S_{%i}(t)+I_{%i}(t)+R_{%i}(t) $'%(i, i, i)
-             , color = 'black')
+    plt.plot(gridT, gridV[i], label = r'$ V_{%i}(t) $'%(i))
+    plt.plot(gridT, gridC[i], label = r'$ C_{%i}(t) $'%(i))
+    plt.plot(gridT, gridB[i], label = r'$ B_{%i}(t) $'%(i))
+    plt.plot(gridT, gridA[i], label = r'$ A_{%i}(t) $'%(i))
     plt.grid(True)
-    plt.title(r'$ Prevalence \ and \ incidence \ of \ SIR $', fontsize = AlvaFontSize)
+    plt.title(r'$ Virion-Tcell-Bcell-Antibody \ (immune \ response \ for \ primary-infection) $', fontsize = AlvaFontSize)
     plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize);
-    plt.ylabel(r'$ Proportion \ of \ population $', fontsize = AlvaFontSize);
-    plt.text(maxT, totalSIR*7.0/6, r'$ R_0 = %f $'%(reprodNum), fontsize = AlvaFontSize)
-    plt.text(maxT, totalSIR*6.0/6, r'$ \gamma = %f $'%(recovRate), fontsize = AlvaFontSize)
-    plt.text(maxT, totalSIR*5.0/6, r'$ \beta = %f $'%(infecRate), fontsize = AlvaFontSize)
-    plt.text(maxT, totalSIR*4.0/6, r'$ \mu = %f $'%(inOutRate), fontsize = AlvaFontSize)
-    plt.text(maxT, totalSIR*3.0/6, r'$ m = %f $'%(mutatRate), fontsize = AlvaFontSize)
+    plt.ylabel(r'$ Cells/ \mu L $', fontsize = AlvaFontSize);
+    plt.text(maxT, totalV*6.0/6, r'$ \Omega = %f $'%(totalV), fontsize = AlvaFontSize)
+    plt.text(maxT, totalV*5.0/6, r'$ \phi = %f $'%(inRateV), fontsize = AlvaFontSize)
+    plt.text(maxT, totalV*4.0/6, r'$ \xi = %f $'%(inRateA), fontsize = AlvaFontSize)
+    plt.text(maxT, totalV*3.0/6, r'$ \mu_b = %f $'%(inOutRateB), fontsize = AlvaFontSize)
     plt.legend(loc = (1,0))
     plt.show()
-'''
+
+# <codecell>
+
+# setting parameter
+timeUnit = 'hour'
+if timeUnit == 'hour':
+    hour = 1; day = 24; 
+elif timeUnit == 'day':
+    day = 1; hour = float(1)/24; 
+    
+totalV = float(5000) # total virion/micro-liter
+inRateV = float(0.16) # intrinsic growth rate/hour
+outRateV = float(6.68*10**(-4)) # virion clearance rate by IgG
+#inOutRateC = float(0.017) # birth rate of CD4 T-cell
+inOutRateB = float(0.037) # birth rate of B-cell
+inRateA = float(0.3) # growth rate of antibody from B-cell (secretion)
+outRateAm = float(0.0048) # out rate of Antibody IgG
+outRateAmV = float(1.67*10**(-4)) # antibody IgG clearance rate by virus
+#actRateC = float(7.0*10**(-6)) # activation rate of CD4 T-cell
+actRateB = float(0.0012) # activation rate of memory B-cell
+
+
+# time boundary and griding condition
+minT = float(0); maxT = float(600*hour);
+totalGPoint_T = int(10**4 + 1);
+gridT = np.linspace(minT, maxT, totalGPoint_T);
+spacingT = np.linspace(minT, maxT, num = totalGPoint_T, retstep = True)
+gridT = spacingT[0]
+dt = spacingT[1]
+
+# space boundary and griding condition
+minX = float(0); maxX = float(0);
+totalGPoint_X = int(1 + 1);
+gridX = np.linspace(minX, maxX, totalGPoint_X);
+gridingX = np.linspace(minX, maxX, num = totalGPoint_X, retstep = True)
+gridX = gridingX[0]
+dx = gridingX[1]
+
+gridV_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridC_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridB_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridA_array = np.zeros([totalGPoint_X, totalGPoint_T])
+
+# initial output condition
+gridV_array[0:1, 0] = float(totalV)/10**3
+gridC_array[0, 0] = float(0)
+gridB_array[:, 0] = float(0)
+gridA_array[:, 0] = float(0)
+
+
+# Runge Kutta numerical solution
+pde_array = np.array([dVdt_array, dCdt_array, dBdt_array, dAdt_array])
+startingOut_Value = np.array([gridV_array, gridC_array, gridB_array, gridA_array])
+gridOut_array = alva.AlvaRungeKutta4ArrayXT(pde_array, startingOut_Value, minX, maxX, totalGPoint_X, minT, maxT, totalGPoint_T)
+
+# plotting
+gridV = gridOut_array[0]  
+gridC = gridOut_array[1]
+gridB = gridOut_array[2]
+gridA = gridOut_array[3]
+
+secondT= np.copy(gridT)
+secondA= np.copy(gridA)
+
+numberingFig = numberingFig + 1;
+for i in range(1):
+    plt.figure(numberingFig, figsize = AlvaFigSize)
+    plt.plot(gridT, gridV[i], label = r'$ V_{%i}(t) $'%(i))
+    plt.plot(gridT, gridC[i], label = r'$ C_{%i}(t) $'%(i))
+    plt.plot(gridT, gridB[i], label = r'$ B_{%i}(t) $'%(i))
+    plt.plot(gridT, gridA[i], label = r'$ A_{%i}(t) $'%(i))
+    plt.grid(True)
+    plt.title(r'$ Virion-Tcell-Bcell-Antibody \ (immune \ response \ for \ secondary-infection) $', fontsize = AlvaFontSize)
+    plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize);
+    plt.ylabel(r'$ Cells/ \mu L $', fontsize = AlvaFontSize);
+    plt.legend(loc = (1,0))
+    plt.show()
 
 # <codecell>
 
 numberingFig = numberingFig + 1;
 plt.figure(numberingFig, figsize = AlvaFigSize)
-plt.pcolormesh(gridT, gridX, gridI)
-plt.title(r'$ Many-strain \ SIR $', fontsize = AlvaFontSize)
+plt.plot(firstT, firstA[0], label = r'$ 1st \ A(t) $')
+plt.plot(secondT + 600, secondA[0], label = r'$ 2nd \ A(t)$')
+plt.title(r'$ Antibody \ (immune \ response \ for \ primary \ and \ secondary \ infections) $', fontsize = AlvaFontSize)
 plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize);
-plt.ylabel(r'$ Strain \ space $', fontsize = AlvaFontSize);
-plt.colorbar()
-plt.show()
-
-# <codecell>
-
-
-# 3D plotting
-# define GridXX function for making 2D-grid from 1D-grid
-def AlvaGridXX(gridX, totalGPoint_Y):
-    gridXX = gridX;
-    for n in range(totalGPoint_Y - 1):
-        gridXX = np.vstack((gridXX, gridX))
-    return gridXX
-# for 3D plotting
-X = AlvaGridXX(gridT, totalGPoint_X); 
-Y = AlvaGridXX(gridX, totalGPoint_T).T; 
-Z = gridI
-numberingFig = numberingFig + 1;
-figure = plt.figure(numberingFig, figsize=(9, 7));
-figure3D = Axes3D(figure)
-figure3D.view_init(30, -80)
-
-figure3D.plot_surface(X, Y, Z, alpha = 0.5)
-plt.xlabel(r'$t \ (time)$', fontsize = AlvaFontSize)
-plt.ylabel(r'$x \ (space)$', fontsize = AlvaFontSize)
+plt.ylabel(r'$ Cells/ \mu L $', fontsize = AlvaFontSize);
+plt.legend(loc = (1,0))
 plt.show()
 
 # <codecell>
