9172c4d338bbea681bf107fbc759eb4838429a5e
Alvason Li <al@A-L.local>
2015-04-12 16:58:09 -0700

    log scale plot

--- bcell-array/bcell-array.py
+++ bcell-array/bcell-array.py
@@ -44,54 +44,7 @@ plt.text(0, 2.0/6,r'$ \frac{\partial A_n(t)}{\partial t} = \
          +\xi B_{n}(t) - \phi_a A_{n}(t)V_{n}(t) - \mu_a A_{n}(t) $', fontsize = 1.2*AlvaFontSize)
 plt.show()
 
-# <codecell>
-
-# setting parameter
-timeUnit = 'hour'
-if timeUnit == 'hour':
-    hour = 1; day = 24; 
-elif timeUnit == 'day':
-    day = 1; hour = float(1)/24; 
-    
-totalV = float(5000) # total virion/micro-liter
-inRateV = float(0.16) # intrinsic growth rate/hour
-outRateV = float(1.67*10**(-4)) # virion clearance rate by IgM
-inOutRateC = float(0.017) # birth rate of CD4 T-cell
-inOutRateB = float(0.037) # birth rate of B-cell
-inRateA = float(0.3) # growth rate of antibody from B-cell (secretion)
-outRateAm = float(0.014) # out rate of Antibody IgM
-outRateAmV = float(4.2*10**(-5)) # antibody IgM clearance rate by virus
-actRateC = float(7.0*10**(-6)) # activation rate of CD4 T-cell
-actRateB = float(6.0*10**(-7)) # activation rate of naive B-cell
-
-
-# time boundary and griding condition
-minT = float(0); maxT = float(600*hour);
-totalGPoint_T = int(10**3 + 1);
-gridT = np.linspace(minT, maxT, totalGPoint_T);
-spacingT = np.linspace(minT, maxT, num = totalGPoint_T, retstep = True)
-gridT = spacingT[0]
-dt = spacingT[1]
-
-# space boundary and griding condition
-minX = float(0); maxX = float(1);
-totalGPoint_X = int(1 + 1);
-gridX = np.linspace(minX, maxX, totalGPoint_X);
-gridingX = np.linspace(minX, maxX, num = totalGPoint_X, retstep = True)
-gridX = gridingX[0]
-dx = gridingX[1]
-
-gridV_array = np.zeros([totalGPoint_X, totalGPoint_T])
-gridC_array = np.zeros([totalGPoint_X, totalGPoint_T])
-gridB_array = np.zeros([totalGPoint_X, totalGPoint_T])
-gridA_array = np.zeros([totalGPoint_X, totalGPoint_T])
-
-# initial output condition
-gridV_array[0:1, 0] = float(totalV)/10**3
-gridC_array[0, 0] = float(0)
-gridB_array[:, 0] = float(0)
-gridA_array[:, 0] = float(0)
-
+# define the partial differential equations
 def dVdt_array(VCBAxt = [], *args):
     # naming
     V = VCBAxt[0]
@@ -148,6 +101,54 @@ def dAdt_array(VCBAxt = [], *args):
         dA_dt_array[xn] = +inRateA*B[xn] - outRateAmV*A[xn]*V[xn] - outRateAm*A[xn]
     return(dA_dt_array)
 
+# <codecell>
+
+# setting parameter
+timeUnit = 'day'
+if timeUnit == 'hour':
+    hour = float(1); day = float(24); 
+elif timeUnit == 'day':
+    day = float(1); hour = float(1)/24; 
+    
+totalV = float(5000) # total virion/micro-liter
+inRateV = float(0.16)/hour # intrinsic growth rate/hour
+outRateV = float(1.67*10**(-4))/hour # virion clearance rate by IgM
+inOutRateC = float(0.017)/hour # birth rate of CD4 T-cell
+inOutRateB = float(0.037)/hour # birth rate of B-cell
+inRateA = float(0.3)/hour # growth rate of antibody from B-cell (secretion)
+outRateAm = float(0.014)/hour # out rate of Antibody IgM
+outRateAmV = float(4.2*10**(-5))/hour # antibody IgM clearance rate by virus
+actRateC = float(7.0*10**(-6))/hour # activation rate of CD4 T-cell
+actRateB = float(6.0*10**(-7))/hour # activation rate of naive B-cell
+
+
+# time boundary and griding condition
+minT = float(0); maxT = float(2000*hour);
+totalGPoint_T = int(10**4 + 1);
+gridT = np.linspace(minT, maxT, totalGPoint_T);
+spacingT = np.linspace(minT, maxT, num = totalGPoint_T, retstep = True)
+gridT = spacingT[0]
+dt = spacingT[1]
+
+# space boundary and griding condition
+minX = float(0); maxX = float(1);
+totalGPoint_X = int(1 + 1);
+gridX = np.linspace(minX, maxX, totalGPoint_X);
+gridingX = np.linspace(minX, maxX, num = totalGPoint_X, retstep = True)
+gridX = gridingX[0]
+dx = gridingX[1]
+
+gridV_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridC_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridB_array = np.zeros([totalGPoint_X, totalGPoint_T])
+gridA_array = np.zeros([totalGPoint_X, totalGPoint_T])
+
+# initial output condition
+gridV_array[0:1, 0] = float(totalV)/10**3
+gridC_array[0, 0] = float(0)
+gridB_array[:, 0] = float(0)
+gridA_array[:, 0] = float(1)
+
 # Runge Kutta numerical solution
 pde_array = np.array([dVdt_array, dCdt_array, dBdt_array, dAdt_array])
 startingOut_Value = np.array([gridV_array, gridC_array, gridB_array, gridA_array])
@@ -185,26 +186,26 @@ for i in range(1):
 # <codecell>
 
 # setting parameter
-timeUnit = 'hour'
+timeUnit = 'day'
 if timeUnit == 'hour':
-    hour = 1; day = 24; 
+    hour = float(1); day = float(24); 
 elif timeUnit == 'day':
-    day = 1; hour = float(1)/24; 
+    day = float(1); hour = float(1)/24; 
     
 totalV = float(5000) # total virion/micro-liter
-inRateV = float(0.16) # intrinsic growth rate/hour
-outRateV = float(6.68*10**(-4)) # virion clearance rate by IgG
-#inOutRateC = float(0.017) # birth rate of CD4 T-cell
-inOutRateB = float(0.037) # birth rate of B-cell
-inRateA = float(0.3) # growth rate of antibody from B-cell (secretion)
-outRateAm = float(0.0048) # out rate of Antibody IgG
-outRateAmV = float(1.67*10**(-4)) # antibody IgG clearance rate by virus
-#actRateC = float(7.0*10**(-6)) # activation rate of CD4 T-cell
-actRateB = float(0.0012) # activation rate of memory B-cell
+inRateV = float(0.16)/hour # intrinsic growth rate/hour
+outRateV = float(6.68*10**(-4))/hour # virion clearance rate by IgG
+inOutRateC = float(0.017)/hour # birth rate of CD4 T-cell
+inOutRateB = float(0.037)/hour # birth rate of B-cell
+inRateA = float(0.3)/hour # growth rate of antibody from B-cell (secretion)
+outRateAm = float(0.0048)/hour # out rate of Antibody IgG
+outRateAmV = float(1.67*10**(-4))/hour # antibody IgG clearance rate by virus
+actRateC = float(7.0*10**(-6))/hour # activation rate of CD4 T-cell
+actRateB = float(0.0012)/hour # activation rate of memory B-cell
 
 
 # time boundary and griding condition
-minT = float(0); maxT = float(600*hour);
+minT = float(0); maxT = float(1000*hour);
 totalGPoint_T = int(10**4 + 1);
 gridT = np.linspace(minT, maxT, totalGPoint_T);
 spacingT = np.linspace(minT, maxT, num = totalGPoint_T, retstep = True)
@@ -228,7 +229,7 @@ gridA_array = np.zeros([totalGPoint_X, totalGPoint_T])
 gridV_array[0:1, 0] = float(totalV)/10**3
 gridC_array[0, 0] = float(0)
 gridB_array[:, 0] = float(0)
-gridA_array[:, 0] = float(0)
+gridA_array[:, 0] = float(1)
 
 
 # Runge Kutta numerical solution
@@ -266,12 +267,25 @@ numberingFig = numberingFig + 1;
 plt.figure(numberingFig, figsize = AlvaFigSize)
 plt.plot(firstT, firstA[0], label = r'$ 1st \ A(t) $', color = 'green', linewidth = 6.0, alpha = 0.3)
 plt.plot(firstT, firstV[0], label = r'$ 1st \ V(t) $', color = 'red')
-plt.plot(secondT + 600, secondA[0], label = r'$ 2nd \ A(t)$', color = 'green', linewidth = 6.0, alpha = 0.3)
-plt.plot(secondT + 600, secondV[0], label = r'$ 2nd \ V(t)$', color = 'red')
+plt.plot(secondT + maxT, secondA[0], label = r'$ 2nd \ A(t)$', color = 'green', linewidth = 6.0, alpha = 0.6)
+plt.plot(secondT + maxT, secondV[0], label = r'$ 2nd \ V(t)$', color = 'red')
 plt.grid(True)
 plt.title(r'$ Antibody \ (immune \ response \ for \ primary \ and \ secondary \ infections) $', fontsize = AlvaFontSize)
 plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize);
 plt.ylabel(r'$ Cells/ \mu L $', fontsize = AlvaFontSize);
+plt.legend(loc = (1,0))
+plt.show()
+
+numberingFig = numberingFig + 1;
+plt.figure(numberingFig, figsize = AlvaFigSize)
+plt.plot(firstT, np.log(firstA[0])/np.log(2), label = r'$ 1st \ A(t) $', color = 'green', linewidth = 6.0, alpha = 0.3)
+#plt.plot(firstT[1:], np.log(firstV[0, 1:])/np.log(2), label = r'$ 1st \ V(t) $', color = 'red')
+plt.plot(secondT + maxT, np.log(secondA[0])/np.log(2), label = r'$ 2nd \ A(t)$', color = 'green', linewidth = 6.0, alpha = 0.6)
+#plt.plot(secondT[1:] + maxT, np.log(secondV[0, 1:])/np.log(2), label = r'$ 2nd \ V(t)$', color = 'red')
+plt.grid(True)
+plt.title(r'$ Antibody \ (immune \ response \ for \ primary \ and \ secondary \ infections) $', fontsize = AlvaFontSize)
+plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize);
+plt.ylabel(r'$ log_2(Cells/ \mu L) $', fontsize = AlvaFontSize);
 plt.legend(loc = (1,0))
 plt.show()
 
