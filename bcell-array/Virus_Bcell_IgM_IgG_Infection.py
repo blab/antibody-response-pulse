f7bde49bbcb9627f187858068e2e0fc16eb6af5e
A L <al@DHCP180101.FHCRC.ORG>
2015-09-25 17:19:53 -0700

    Antibody landscape

---
+++ bcell-array/Virus_Bcell_IgM_IgG_Infection.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# # Antibody Response Pulse
+# https://github.com/blab/antibody-response-pulse
+# 
+# ### B-cells evolution --- cross-reactive antibody response after influenza virus infection or vaccination
+# ### Adaptive immune response for repeated infection
+
+# <codecell>
+
+'''
+author: Alvason Zhenhua Li
+date:   04/09/2015
+'''
+%matplotlib inline
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+import alva_machinery_event as alva
+
+AlvaFontSize = 23
+AlvaFigSize = (15, 5)
+numberingFig = 0
+
+# plotting
+dir_path = '/Users/al/Desktop/GitHub/antibody-response-pulse/bcell-array/figure'
+file_name = 'Virus-Bcell-Antibody'
+figure_name = '-equation'
+file_suffix = '.png'
+save_figure = os.path.join(dir_path, file_name + figure_name + file_suffix)
+
+numberingFig = numberingFig + 1
+plt.figure(numberingFig, figsize=(12, 5))
+plt.axis('off')
+plt.title(r'$ Virus-Bcell-IgM-IgG \ equations \ (antibody-response \ for \ repeated-infection) $'
+          , fontsize = AlvaFontSize)
+plt.text(0, 7.0/9, r'$ \frac{\partial V_n(t)}{\partial t} = \
+         +\mu_{v} V_{n}(t)(1 - \frac{V_n(t)}{V_{max}}) - \phi_{m} M_{n}(t) V_{n}(t) - \phi_{g} G_{n}(t) V_{n}(t) $'
+         , fontsize = 1.2*AlvaFontSize)
+plt.text(0, 5.0/9, r'$ \frac{\partial B_n(t)}{\partial t} = \
+         +\mu_{b} + (\beta_{m} + \beta_{g}) V_{n}(t) B_{n}(t) - \mu_{b} B_{n}(t) $'
+         , fontsize = 1.2*AlvaFontSize)
+plt.text(0, 3.0/9,r'$ \frac{\partial M_n(t)}{\partial t} = \
+         +\xi_{m} B_{n}(t) - \phi_{m} M_{n}(t) V_{n}(t) - \mu_{m} M_{n}(t) $'
+         , fontsize = 1.2*AlvaFontSize)
+plt.text(0, 1.0/9,r'$ \frac{\partial G_n(t)}{\partial t} = \
+         +\xi_{g} B_{n}(t) - \phi_{g} G_{n}(t) V_{n}(t) - \mu_{g} G_{n}(t) \
+         + m \frac{G_{i-1}(t) - 2G_i(t) + G_{i+1}(t)}{(\Delta i)^2} $'
+         , fontsize = 1.2*AlvaFontSize)
+
+plt.savefig(save_figure, dpi = 100)
+plt.show()
+
+
+# define the V-M-G partial differential equations
+def dVdt_array(VBMGxt = [], *args):
+    # naming
+    V = VBMGxt[0]
+    B = VBMGxt[1]
+    M = VBMGxt[2]
+    G = VBMGxt[3]
+    x_totalPoint = VBMGxt.shape[1]
+    # there are n dSdt
+    dV_dt_array = np.zeros(x_totalPoint)
+    # each dSdt with the same equation form
+    dV_dt_array[:] = +inRateV*V[:]*(1 - V[:]/maxV) - killRateVm*M[:]*V[:] - killRateVg*G[:]*V[:]
+    return(dV_dt_array)
+
+def dBdt_array(VBMGxt = [], *args):
+    eName = np.zeros([totalPoint_X])
+    eName[0] = 1.0/100/day 
+    eName[1] = alva.eventName
+#    eName[2] = 1.0/100 
+#    eName[3] = 1.0/100
+    # naming
+    V = VBMGxt[0]
+    B = VBMGxt[1]
+    M = VBMGxt[2]
+    G = VBMGxt[3]
+    x_totalPoint = VBMGxt.shape[1]
+    # there are n dSdt
+    dB_dt_array = np.zeros(x_totalPoint)
+    # each dSdt with the same equation form
+    dB_dt_array[:] = +inRateB + (actRateBm + eName[:])*V[:]*B[:] - outRateB*B[:]
+    return(dB_dt_array)
+
+def dMdt_array(VBMGxt = [], *args):
+    eName = np.zeros([totalPoint_X])
+    eName[0] = 1.0/100/day 
+    eName[1] = alva.eventName
+#    eName[2] = 1.0/100 
+#    eName[3] = 1.0/100
+    # naming
+    V = VBMGxt[0]
+    B = VBMGxt[1]
+    M = VBMGxt[2]
+    G = VBMGxt[3]
+    x_totalPoint = VBMGxt.shape[1]
+    # there are n dSdt
+    dM_dt_array = np.zeros(x_totalPoint)
+    # each dSdt with the same equation form
+    dM_dt_array[:] = +inRateM*B[:]*actRateBm/(actRateBm + eName[:]) - consumeRateM*M[:]*V[:] - outRateM*M[:]
+    return(dM_dt_array)
+
+def dGdt_array(VBMGxt = [], *args):
+    # naming
+    V = VBMGxt[0]
+    B = VBMGxt[1]
+    M = VBMGxt[2]
+    G = VBMGxt[3]
+    x_totalPoint = VBMGxt.shape[1]
+    # there are n dSdt
+    dG_dt_array = np.zeros(x_totalPoint)
+    # each dSdt with the same equation form
+        # each dIdt with the same equation form
+    Gcopy = np.copy(G)
+    centerX = Gcopy[:]
+    leftX = np.roll(Gcopy[:], 1)
+    rightX = np.roll(Gcopy[:], -1)
+    leftX[0] =centerX[0]
+    rightX[-1] = centerX[-1]
+    dG_dt_array[:] = +inRateG*B[:] - consumeRateG*G[:]*V[:] - outRateG*G[:] \
+                     + mutatRate*(leftX[:] - 2*centerX[:] + rightX[:])/(dx**2)
+    return(dG_dt_array)
+
+# <codecell>
+
+# setting parameter
+timeUnit = 'year'
+if timeUnit == 'hour':
+    hour = float(1)
+    day = float(24)
+elif timeUnit == 'day':
+    day = float(1)
+    hour = float(1)/24 
+elif timeUnit == 'year':
+    year = float(1)
+    day = float(1)/365
+    hour = float(1)/24/365 
+    
+maxV = float(1000) # max virus/milli-liter
+inRateV = 6.5*maxV/10**4/day # in-rate of virus
+killRateVm = 1*maxV/10**5/day # kill-rate of virus by antibody-IgM
+killRateVg = killRateVm # kill-rate of virus by antibody-IgG
+
+inRateB = 3*maxV/10**4/day # in-rate of B-cell
+outRateB = inRateB # out-rate of B-cell
+actRateBm = killRateVm # activation rate of naive B-cell
+#actRateBg = float(1)/10**2 # activation rate of memory B-cell
+
+inRateM = maxV/10**2/day  # in-rate of antibody-IgM from naive B-cell
+outRateM = inRateM  # out-rate of antibody-IgM from naive B-cell
+consumeRateM = killRateVm # consume-rate of antibody-IgM by cleaning virus
+
+inRateG = inRateM/10 # in-rate of antibody-IgG from memory B-cell
+outRateG = outRateM/100 # out-rate of antibody-IgG from memory B-cell
+consumeRateG = consumeRateM  # consume-rate of antibody-IgG by cleaning virus
+mutatRate = float(1)/10**1/day # mutation rate
+# time boundary and griding condition
+minT = float(0)
+maxT = float(120*28*day)
+totalPoint_T = int(5*10**4 + 1)
+gT = np.linspace(minT, maxT, totalPoint_T)
+spacingT = np.linspace(minT, maxT, num = totalPoint_T, retstep = True)
+gT = spacingT[0]
+dt = spacingT[1]
+
+# space boundary and griding condition
+minX = float(0)
+maxX = float(5)
+totalPoint_X = int(maxX - minX + 1)
+gX = np.linspace(minX, maxX, totalPoint_X)
+gridingX = np.linspace(minX, maxX, num = totalPoint_X, retstep = True)
+gX = gridingX[0]
+dx = gridingX[1]
+gV_array = np.zeros([totalPoint_X, totalPoint_T])
+gB_array = np.zeros([totalPoint_X, totalPoint_T])
+gM_array = np.zeros([totalPoint_X, totalPoint_T])
+gG_array = np.zeros([totalPoint_X, totalPoint_T])
+# initial output condition
+gV_array[1, 0] = float(1)
+gB_array[:, 0] = float(0)
+gM_array[0, 0] = float(0)
+gG_array[0, 0] = float(0)
+
+event_tn_In = np.array([[0*day, 1/10**2/day], [14*day, 1/1/day]])
+
+# Runge Kutta numerical solution
+pde_array = np.array([dVdt_array, dBdt_array, dMdt_array, dGdt_array])
+initial_Out = np.array([gV_array, gB_array, gM_array, gG_array])
+gOut_array = alva.AlvaRungeKutta4XT(pde_array, initial_Out, minX, maxX, totalPoint_X, minT, maxT, totalPoint_T, event_tn_In)
+
+# plotting
+gV = gOut_array[0]  
+gB = gOut_array[1] 
+gM = gOut_array[2]
+gG = gOut_array[3]
+
+numberingFig = numberingFig + 1
+for i in range(totalPoint_X):
+    figure_name = '-response-%i'%(i)
+    figure_suffix = '.png'
+    save_figure = os.path.join(dir_path, file_name + figure_name + file_suffix)
+    plt.figure(numberingFig, figsize = AlvaFigSize)
+    plt.plot(gT, gV[i], color = 'red', label = r'$ V_{%i}(t) $'%(i))
+    plt.plot(gT, gM[i], color = 'blue', label = r'$ IgM_{%i}(t) $'%(i))
+    plt.plot(gT, gG[i], color = 'green', label = r'$ IgG_{%i}(t) $'%(i))
+    plt.plot(gT, gM[i] + gG[i], color = 'gray', linewidth = 5.0, alpha = 0.5, linestyle = 'dashed'
+             , label = r'$ IgM_{%i}(t) + IgG_{%i}(t) $'%(i, i))
+    plt.grid(True, which = 'both')
+    plt.title(r'$ Antibody \ due \ to \ Virus-{%i} $'%(i), fontsize = AlvaFontSize)
+    plt.xlabel(r'$time \ (%s)$'%(timeUnit), fontsize = AlvaFontSize)
+    plt.ylabel(r'$ Neutralization \ \ titer $', fontsize = AlvaFontSize)
+    plt.xlim([minT, maxT])
+    plt.xticks(fontsize = AlvaFontSize*0.6)
+    plt.yticks(fontsize = AlvaFontSize*0.6) 
+    plt.ylim([2**0, 2**12])
+    plt.yscale('log', basey = 2)
+    plt.legend(loc = (1,0), fontsize = AlvaFontSize)
+    plt.savefig(save_figure, dpi = 100)
+    plt.show()
+
+# <codecell>
+
+test_time = int(totalPoint_T*(2.0/10)) + totalPoint_T*16*day/(maxT - minT)
+
+# plotting
+figure_name = '-antibody-landscape'
+figure_suffix = '.png'
+save_figure = os.path.join(dir_path, file_name + figure_name + file_suffix)
+
+plt.figure(numberingFig, figsize = AlvaFigSize)
+plt.plot(gX, gM[:, test_time] + gG[:, test_time], marker = 'o', markersize = 20, color = 'red', alpha = 0.6)
+plt.fill_between(gX, gM[:, test_time] + gG[:, test_time], color = 'green', alpha=0.3)
+plt.grid(True, which = 'both')
+plt.title(r'$ Antibody \ Landscape $', fontsize = AlvaFontSize)
+plt.xlabel(r'$ Virus \ space \ (Antigenic-distance) $', fontsize = AlvaFontSize)
+plt.ylabel(r'$ Neutralization \ \ titer $', fontsize = AlvaFontSize)
+plt.xlim([minX, maxX])
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
+plt.ylim([2**0, 2**10])
+plt.yscale('log', basey = 2)
+plt.legend(loc = (1,0), fontsize = AlvaFontSize)
+plt.savefig(save_figure, dpi = 100)
+plt.show()
+
+# <codecell>
+
+
---
+++ bcell-array/alva_machinery_event.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# # Infectious Pulse
+# https://github.com/blab/antibody-response-pulse/
+# 
+# ## Homemade machinery for solving partial differential equations
+# ### Runge-kutta algorithm for a array of coupled partial differential equation 
+
+# <codecell>
+
+'''
+author: Alvason Zhenhua Li
+date:   03/23/2015
+
+Home-made machinery for solving partial differential equations
+'''
+import numpy as np
+
+# define RK4 for an array (3, n) of coupled differential equations
+def AlvaRungeKutta4XT(pde_array, initial_Out, minX_In, maxX_In, totalPoint_X, minT_In, maxT_In, totalPoint_T, event_tn_In):
+    global eventName
+    # primary size of pde equations
+    outWay = pde_array.shape[0]
+    # initialize the whole memory-space for output and input
+    inWay = 1; # one layer is enough for storing "x" and "t" (only two list of variable)
+    # define the first part of array as output memory-space
+    gOutIn_array = np.zeros([outWay + inWay, totalPoint_X, totalPoint_T])
+    # loading starting output values
+    for i in range(outWay):
+        gOutIn_array[i, :, :] = initial_Out[i, :, :]
+    # griding input X value  
+    gridingInput_X = np.linspace(minX_In, maxX_In, num = totalPoint_X, retstep = True)
+    # loading input values to (define the final array as input memory-space)
+    gOutIn_array[-inWay, :, 0] = gridingInput_X[0]
+    # step-size (increment of input X)
+    dx = gridingInput_X[1]
+    # griding input T value  
+    gridingInput_T = np.linspace(minT_In, maxT_In, num = totalPoint_T, retstep = True)
+    # loading input values to (define the final array as input memory-space)
+    gOutIn_array[-inWay, 0, :] = gridingInput_T[0]
+    # step-size (increment of input T)
+    dt = gridingInput_T[1]
+    # starting
+    # initialize the memory-space for local try-step 
+    dydt1_array = np.zeros([outWay, totalPoint_X])
+    dydt2_array = np.zeros([outWay, totalPoint_X])
+    dydt3_array = np.zeros([outWay, totalPoint_X])
+    dydt4_array = np.zeros([outWay, totalPoint_X])
+    # initialize the memory-space for keeping current value
+    currentOut_Value = np.zeros([outWay, totalPoint_X])
+    for tn in range(totalPoint_T - 1):
+        # secondary period
+        eventName = event_tn_In[0, 1] 
+        if tn > int(totalPoint_T*(event_tn_In[1, 0]/(maxT_In - minT_In))):
+            eventName = event_tn_In[1, 1]
+        ### timepoint of 2nd virus infection
+        if tn == int(totalPoint_T*(1.0/10)):
+            gOutIn_array[0, 1, tn] = 1.0 # set which virus infection 
+        # setting virus = 0 if virus < 1
+        if gOutIn_array[0, 1, tn] < 1.0:
+            gOutIn_array[0, 1, tn] = 0.0
+        ### timepoint of 3rd virus infection
+        if tn == int(totalPoint_T*(2.0/10)):
+            gOutIn_array[0, 2, tn] = 1.0 # set which virus infection 
+        # setting virus = 0 if virus < 1
+        if gOutIn_array[0, 2, tn] < 1.0:
+            gOutIn_array[0, 2, tn] = 0.0
+        ### timepoint of 4th virus infection
+        if tn == int(totalPoint_T*(3.0/10)):
+            gOutIn_array[0, 3, tn] = 1.0 # set which virus infection 
+        # setting virus = 0 if virus < 1
+        if gOutIn_array[0, 3, tn] < 1.0:
+            gOutIn_array[0, 3, tn] = 0.0       
+        # keep initial value at the moment of tn
+        currentOut_Value[:, :] = np.copy(gOutIn_array[:-inWay, :, tn])
+        currentIn_T_Value = np.copy(gOutIn_array[-inWay, 0, tn])
+        # first try-step
+        for i in range(outWay):
+            for xn in range(totalPoint_X):
+                dydt1_array[i, xn] = pde_array[i](gOutIn_array[:, :, tn])[xn] # computing ratio   
+        gOutIn_array[:-inWay, :, tn] = currentOut_Value[:, :] + dydt1_array[:, :]*dt/2 # update output
+        gOutIn_array[-inWay, 0, tn] = currentIn_T_Value + dt/2 # update input
+        # second half try-step
+        for i in range(outWay):
+            for xn in range(totalPoint_X):
+                dydt2_array[i, xn] = pde_array[i](gOutIn_array[:, :, tn])[xn] # computing ratio   
+        gOutIn_array[:-inWay, :, tn] = currentOut_Value[:, :] + dydt2_array[:, :]*dt/2 # update output
+        gOutIn_array[-inWay, 0, tn] = currentIn_T_Value + dt/2 # update input
+        # third half try-step
+        for i in range(outWay):
+            for xn in range(totalPoint_X):
+                dydt3_array[i, xn] = pde_array[i](gOutIn_array[:, :, tn])[xn] # computing ratio   
+        gOutIn_array[:-inWay, :, tn] = currentOut_Value[:, :] + dydt3_array[:, :]*dt # update output
+        gOutIn_array[-inWay, 0, tn] = currentIn_T_Value + dt # update input
+        # fourth try-step
+        for i in range(outWay):
+            for xn in range(totalPoint_X):
+                dydt4_array[i, xn] = pde_array[i](gOutIn_array[:, :, tn])[xn] # computing ratio 
+        # solid step (update the next output) by accumulate all the try-steps with proper adjustment
+        gOutIn_array[:-inWay, :, tn + 1] = currentOut_Value[:, :] + dt*(dydt1_array[:, :]/6 
+                                                                                      + dydt2_array[:, :]/3 
+                                                                                      + dydt3_array[:, :]/3 
+                                                                                      + dydt4_array[:, :]/6)
+        # restore to initial value
+        gOutIn_array[:-inWay, :, tn] = np.copy(currentOut_Value[:, :])
+        gOutIn_array[-inWay, 0, tn] = np.copy(currentIn_T_Value)
+        # end of loop
+    return (gOutIn_array[:-inWay, :])
+
+# <codecell>
+
+# min-max sorting
+def AlvaMinMax(data):
+    totalDataPoint = np.size(data)
+    minMaxListing = np.zeros(totalDataPoint)   
+    for i in range(totalDataPoint):
+        # searching the minimum in current array
+        jj = 0 
+        minMaxListing[i] = data[jj] # suppose the 1st element [0] of current data-list is the minimum
+        for j in range(totalDataPoint - i):
+            if data[j] < minMaxListing[i]: 
+                minMaxListing[i] = data[j]
+                jj = j # recording the position of selected element
+        # reducing the size of searching zone (removing the minmum from current array)
+        data = np.delete(data, jj)
+    return (minMaxListing)
+
